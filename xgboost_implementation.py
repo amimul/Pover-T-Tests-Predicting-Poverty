8f908b68c3c258d9fd787b56108acad6f3e287e1
Masesi <masesidan@gmail.com>
2018-01-22 15:50:38 +0300

    XGBoost

--- xgboost_implementation.py
+++ xgboost_implementation.py
@@ -22,10 +22,68 @@ c_test = pd.read_csv('test_data/C_hhold_test.csv', index_col='id')
 
 a_train.head()
 
+# preprocess the data
+
+# standardize features
+def standardize(df, numeric_only=True):
+    numeric = df.select_dtypes(include=['int64', 'float64'])
+
+    # subtract mean and divide by std
+    df[numeric.columns] = (numeric - numeric.mean()) / numeric.std()
+
+    return df
+
+def per_process_data(df, enforce_cols=None):
+    print("Input shape: \t {}".format(df.shape))
+
+    df = standardize(df)
+    print("After standardization {}".format(df.shape))
+
+    # create dummy variables for categoricals
+    df = pd.get_dummies(df)
+    print("After converting categoricals: \t {}".format(df.shape))
+
+
+
+
 # function to create XGBoost models and perform cross-validation.
 def modelfit(alg, dtrain, predictors, useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
 
     if useTrainCV:
         xgb_param = alg.get_xgb_params()
         xgtrain = xgb.DMatrix(dtrain[predictors].values, label=dtrain[target].values)
-        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds, metrics='auc', early_stopping_rounds=early_stopping_rounds, show_progress=False)
+        alg.set_params(n_estimators=cvresult.shape[0])
+
+    # fit the algorithm on the data
+    alg.fit(dtrain[predictors], dtrain['target'], eval_metric='auc')
+
+    # predict training set:
+    dtrain_predictions = alg.predict(dtrain[predictors])
+    dtrain_predprob = alg.predict_proba(dtrain[predictors])[:, 1]
+
+    # print model report
+    print("\nModel Report")
+    print("Accuracy : %.4g" % metrics.accuracy_score(dtrain['target'].values, dtrain_predictions))
+    print('AUC Score (Train) : %f' % metrics.roc_auc_score(dtrain['target'], dtrain_predprob))
+
+    feat_imp = pd.Series(alg.booster().get_fscore()).sort_values(ascending=False)
+    feat_imp.plot(kind='bar', title='Feature Importance')
+    plt.ylabel('Feature Importance Score')
+
+# choose all predictors except target & IDcols
+predictors = [x for x in train.columns if x not in [target, IDcol]]
+xgb1 = XGBClassifier(
+    learning_rate = 0.1,
+    n_estimators = 1000,
+    max_depth = 5,
+    min_child_weight = 1,
+    gamma = 0,
+    subsample = 0.8,
+    colsample_bytree = 0.8,
+    objective = 'binary:logistic',
+    nthread = 4,
+    scale_pos_weight = 1,
+    seed = 27
+)
+modelfit(xgb1, train, predictors)
